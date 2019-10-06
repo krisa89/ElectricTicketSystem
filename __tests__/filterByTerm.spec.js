caac130165782b20e722143f94d9784c40ba0d1b
krisa89 <krisa89@gmail.com>
2019-10-06 18:30:53 +0200

    Substract function is added

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,8 +1,15 @@
 
 const { add } = require('./../file1')  
+const { substract } = require('./../file1')  
 
 describe("Filter Function",() => {
     test("It should be the test name", () => {
         expect(add(2,3)).toEqual(5)
+    })
+})
+
+describe("substract",() => {
+    test("Substract test", () => {
+        expect(substract(4,3)).toEqual(1)
     })
 })
