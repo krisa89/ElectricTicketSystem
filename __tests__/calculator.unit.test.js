f2c98d225c02fd1c73cdf3ba6ab1252cd1e9c253
krisa89 <krisa89@gmail.com>
2019-10-08 23:33:01 +0200

    BeforeEach test is added

--- __tests__/calculator.unit.test.js
+++ __tests__/calculator.unit.test.js
@@ -1,23 +1,25 @@
 
 const Calculator = require("./../calculator")
 
+var calculator
+beforeEach(() => {
+    calculator = new Calculator()    
+})
+
 describe("Add",() => {
     test("Add test", () => {
-        const calculator = new Calculator()
         expect(calculator.add(2,3)).toEqual(5)
     })
 })
 
 describe("substract",() => {
     test("Substract test", () => {
-        const calculator = new Calculator()
         expect(calculator.sub(4,3)).toEqual(1)
     })
 })
 
 describe("multiply",() => {
     test("Multiply test", () => {
-        const calculator = new Calculator()
         expect(calculator.multiply(4,3)).toEqual(12)
     })
 })
